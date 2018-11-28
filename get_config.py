e27fd8631e500ad2101ba0f7d3fd1ccb455ff786
dikkini <dikkini@gmail.com>
2018-11-29 01:21:24 +0300

    w/o utils, all method in

--- get_config.py
+++ get_config.py
@@ -1,15 +1,15 @@
 import base64
-import json
+import calendar
 import logging
 import os
 import shutil
 import time
+import uuid
+from datetime import datetime
 
 import requests
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
-
-from utils import gen_sec_token
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -19,6 +19,8 @@ logging.basicConfig(
 logger = logging.getLogger("get_config_script")
 
 DIRECTORY_TO_WATCH = "/tmp/dfnvpn_ansible"
+
+
 # DIRECTORY_TO_WATCH = "./dfn"
 
 
@@ -136,6 +138,47 @@ def process_file(config_file_name):
     else:
         logger.debug("API Response IS OK! Delete file.")
         vpn_config.delete()
+
+
+def gen_sec_token() -> str:
+    token = ""
+    ruuid = str(uuid.uuid4())
+    ruuid = ruuid.replace("-", "")
+    ruuid_len = len(ruuid)
+    r4 = str(random_x(1, ruuid_len))
+    token += ruuid
+    unixtime = get_unixtime()
+    unixtime_divided = int(unixtime) / int(r4)
+    unixtime_divided_rounded = "%.10f" % (unixtime_divided)
+    unixtime_divided_len = str(len(str(unixtime_divided_rounded)))
+    if len(unixtime_divided_len) == 1:
+        unixtime_divided_len = "0" + str(unixtime_divided_len)
+    left_token = token[:int(r4)]
+    center_token = str(unixtime_divided_rounded)
+    right_token = token[int(r4):]
+    token = left_token + center_token + right_token
+    if len(r4) == 1:
+        r4 = "0" + str(r4)
+    token = str(r4) + str(unixtime_divided_len) + token
+    return token
+
+
+def random_with_n_digits(n):
+    range_start = 10 ** (n - 1)
+    range_end = (10 ** n) - 1
+    from random import randint
+    return randint(range_start, range_end)
+
+
+def random_x(minX, maxX):
+    import random
+    return random.randint(minX, maxX)
+
+
+def get_unixtime() -> int:
+    d = datetime.utcnow()
+    unixtime = calendar.timegm(d.utctimetuple())
+    return unixtime
 
 
 if __name__ == '__main__':
